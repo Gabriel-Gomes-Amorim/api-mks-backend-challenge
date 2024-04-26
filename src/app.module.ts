829448eaeed689b750f042633c8873770f4daf88
Gabriel-Gomes-Amorim <gomez.amorim18@hotmail.com>
2024-04-26 19:04:22 -0300

    refactor: removing unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from './database/database.providers';
@@ -14,7 +12,5 @@ import { UserModule } from './user/user.module';
     TypeOrmModule.forRoot(dataSourceOptions),
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
